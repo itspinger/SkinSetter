c8a7f17311d2465ded1eeed7c7dd3d192c4840e2
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-07-29 19:01:58 +0200

    Write onEnable with the skin property

--- src/main/java/net/pinger/skinsetter/SkinSetter.java
+++ src/main/java/net/pinger/skinsetter/SkinSetter.java
@@ -1,4 +1,46 @@
 package net.pinger.skinsetter;
 
-public class SkinSetter {
+import net.pinger.disguise.DisguiseAPI;
+import net.pinger.disguise.Skin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkinSetter extends JavaPlugin {
+
+    private Skin skin;
+
+    @Override
+    public void onEnable() {
+        // This might happen with new versions
+        // Or with versions older than 1.8
+        // And the plugin will not have it's core functionality
+        // So it is advised to disable it
+        if (DisguiseAPI.getProvider() == null) {
+            getLogger().info("Failed to find the provider for this version");
+            getLogger().info("Disabling...");
+
+            // Disable the plugin
+            this.getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        // Fetch the url from the config
+        String skinUrl = getConfig().getString("skin");
+
+        // Using the SkinManager
+        // Try to get the skin from the url
+        // Or use the fallback skin (Tylarzz)
+        DisguiseAPI.getSkinManager().getFromImage(skinUrl, response -> {
+            // Check if the response was successful
+            if (!response.success()) {
+                skin = DisguiseAPI.getSkinManager().getFromMojang("Tylarzz");
+                return;
+            }
+
+            skin = response.get();
+        });
+    }
+
+    public Skin getSkin() {
+        return skin;
+    }
 }
